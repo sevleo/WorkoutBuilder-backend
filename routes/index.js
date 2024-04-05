57a4cd06c85bd2f8b6ebc12251521b820ddf26fa
Seva <seva.leonov@hotmail.com>
2024-04-05 17:10:01 +0700

    Add route comments

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,28 @@ router.use(passport.session());
 // Require controller modules
 const navigation_controller = require("../controllers/navigationController");
 
+// Pages
 router.get("/", navigation_controller.home);
 router.get("/sign-up", navigation_controller.signup);
+
+// Google login API
 router.get(
   "/login/federated/google",
   navigation_controller.loginFederatedGoogle
 );
 router.get("/oauth2/redirect/google", navigation_controller.redirectGoogle);
 router.get("/redirect", navigation_controller.redirectFrontend);
+
+// Password login API
 router.post("/login/password", navigation_controller.loginPassword);
+
+// Check login status API
 router.get("/check-login", navigation_controller.checkLogin);
+
+// Logout API
 router.get("/log-out", navigation_controller.logout);
+
+// SignUp API
 router.post("/sign-up", navigation_controller.signUp);
 
 module.exports = router;
