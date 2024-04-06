10521cbc445fb1f1a679bd8977d4195c1b395120
Seva <seva.leonov@hotmail.com>
2024-04-06 12:17:04 +0700

    Update new flow post request

--- controllers/flowController.js
+++ controllers/flowController.js
@@ -2,11 +2,12 @@ const asyncHandler = require("express-async-handler");
 const Flow = require("../models/flow");
 
 exports.createFlow = asyncHandler(async (req, res, next) => {
+  console.log(req.body);
   try {
     const newFlow = new Flow({
-      userId: "123",
-      flowName: "flow name",
-      difficulty: "easy",
+      userId: req.body.userId,
+      flowName: req.body.flowName,
+      difficulty: req.body.flowDifficulty,
     });
     await newFlow.save();
     res.status(200).json({ message: "Flow added" });
