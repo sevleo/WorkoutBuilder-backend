ae3dd35890f155de23d20d3c1edac3fa65ed7aaf
Seva <seva.leonov@hotmail.com>
2024-04-06 12:17:17 +0700

    Remove console log

--- controllers/flowController.js
+++ controllers/flowController.js
@@ -2,7 +2,6 @@ const asyncHandler = require("express-async-handler");
 const Flow = require("../models/flow");
 
 exports.createFlow = asyncHandler(async (req, res, next) => {
-  console.log(req.body);
   try {
     const newFlow = new Flow({
       userId: req.body.userId,
