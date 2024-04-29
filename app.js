d22c0ea1ffc9e1c7ca99d8994ad20dd72e5e7983
Seva <seva.leonov@hotmail.com>
2024-04-29 22:34:27 +0700

    Update PORT

--- app.js
+++ app.js
@@ -43,4 +43,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/", indexRouter);
 
-app.listen(3001, () => console.log("app listening on port 3001!"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("app listening on port 3001!"));
