c627221152a631f5ae5c71f790398c2ec0138db8
Seva <seva.leonov@hotmail.com>
2024-05-01 14:49:17 +0700

    Add trust proxy

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.set("views", __dirname);
 app.set("views", path.join(__dirname, "views"));
 
 app.set("view engine", "ejs");
+app.set("trust proxy", 1);
+
 app.use(
   session({
     secret: "keyboard cat",
